1d6b1fdeb4bef6e738c722a96cabdbdcad713d6c
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2024-07-12 23:02:55 -0600

    Actualzaicon masiva de maritzel

--- routes/web.php
+++ routes/web.php
@@ -73,20 +73,27 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('operadores/show/pagos/{id}', [App\Http\Controllers\OperadorController::class, 'show_pagos'])->name('show_pagos.operadores');
 
     // ==================== C O T I Z A C I O N E S ====================
-    Route::get('/cotizaciones/index', [App\Http\Controllers\CotizacionesController::class, 'index'])->name('index.cotizaciones');
+    Route::get('cotizaciones/index', [App\Http\Controllers\CotizacionesController::class, 'index'])->name('index.cotizaciones');
+
     Route::get('cotizaciones/create', [App\Http\Controllers\CotizacionesController::class, 'create'])->name('create.cotizaciones');
+
     Route::post('cotizaciones/store', [App\Http\Controllers\CotizacionesController::class, 'store'])->name('store.cotizaciones');
+
     Route::get('cotizaciones/edit/{id}', [App\Http\Controllers\CotizacionesController::class, 'edit'])->name('edit.cotizaciones');
+
     Route::patch('cotizaciones/update/{id}', [App\Http\Controllers\CotizacionesController::class, 'update'])->name('update.cotizaciones');
 
-    Route::get('cotizaciones/pdf/{id}', [App\Http\Controllers\CotizacionesController::class, 'pdf'])->name('pdf.cotizaciones');
-
     Route::patch('cotizaciones/update/estatus/{id}', [App\Http\Controllers\CotizacionesController::class, 'update_estatus'])->name('update_estatus.cotizaciones');
-    Route::patch('cotizaciones/update/tipo/{id}', [App\Http\Controllers\CotizacionesController::class, 'update_cambio'])->name('update_cambio.cotizaciones');
 
-    Route::get('subclientes/{clienteId}', [App\Http\Controllers\CotizacionesController::class, 'getSubclientes']);
 
-    Route::patch('cotizaciones/cambiar/empresa/{id}', [App\Http\Controllers\CotizacionesController::class, 'cambiar_empresa'])->name('cambiar_empresa.cotizaciones');
+        // ==================== C O T I Z A C I O N E S ====================
+        Route::get('servicios', [App\Http\Controllers\ServiciosController::class, 'index'])->name('index.servicios');
+        Route::post('servicios/create', [App\Http\Controllers\ServiciosController::class, 'store'])->name('store.servicios');
+        Route::patch('servicios/update/{id}', [App\Http\Controllers\ServiciosController::class, 'update'])->name('update.servicios');
+
+        Route::get('categorias', [App\Http\Controllers\CategoriasController::class, 'index'])->name('index.categorias');
+        Route::post('clients/create', [App\Http\Controllers\CategoriasController::class, 'store'])->name('store.categorias');
+        Route::patch('categorias/update/{id}', [App\Http\Controllers\CategoriasController::class, 'update'])->name('update.categorias');
 
     /*|--------------------------------------------------------------------------
     |Configuracion
